206c8eb66ffe1cda739e216a6b09dc5c7b3677bf
Alexisssssssss <alexis.georges@viacesi.fr>
2019-01-21 10:11:40 +0100

    Structure Basique
    Model Controller Requests and Migrations.

---
+++ bde-cesi-nice/app/Evenements.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Evenements extends Model
+{
+    //
+    protected $table = 'evenements';
+    
+    public $timestamps = false;
+}
---
+++ bde-cesi-nice/database/migrations/2019_01_18_141208_create_utilisateurs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUtilisateursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilisateurs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom_utilisateur');
+            $table->string('prenom_utilisateur');
+            $table->string('email_utilisateur');
+            $table->enum('campus_utilisateur', ['Sophia Antipolis','Aix-en-provence','Montpellier','Toulouse','Pau','Bordeaux','Grenoble','Lyon','Dijon','La Rochelle','Angoulême','Orléans','Le Mans', 'Saint-Nazaire', 'Nantes', 'Brest', 'Caen', 'Rouen', 'Paris Nanterre', 'Reims', 'Arras', 'Lille', 'Nancy', 'Strasbourg']);
+            $table->string('password_utilisateur');
+            $table->boolean('d_etudiant_utilisateur');
+            $table->boolean('d_bde_utilisateur');
+            $table->boolean('d_salarie_utilisateur');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilisateurs');
+    }
+}
