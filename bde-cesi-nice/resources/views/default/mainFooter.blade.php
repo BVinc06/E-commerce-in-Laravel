6b739f2fd51aa3dde7c5e1d66f12aa537dea6f41
Alexisssssssss <33146933+Alexisssssssss@users.noreply.github.com>
2019-01-29 11:14:13 +0100

    Update mainFooter.blade.php

--- bde-cesi-nice/resources/views/default/mainFooter.blade.php
+++ bde-cesi-nice/resources/views/default/mainFooter.blade.php
@@ -4,22 +4,14 @@
 			Toute l'actualité sur <a href="https://www.facebook.com/BdeCesiNice/?ref=br_rs">notre page facebook</a>
 		</div>
 
-		<div class="col-sm">
-<<<<<<< HEAD
-			Nos partenaires :
-		</div>
-
-		<div class="col-sm">
-			Les membres du BDE 2019 :
-		</div>
-=======
+	
 			A savoir :<br>
 			<a href="/Projet_Web_A2/bde-cesi-nice_vincent/public/legal">Mentions légales</a>
 		</div>
 
 		<div class="col-sm">
 			Bienvenue de la part de l'équipe BDE 2019 !		</div>
->>>>>>> master
+
 
 	</div>
 </div>
