c859e0f8dc0ebaf84488bcd062d9a1cfd55d73ef
PaulGouze <33146854+PaulGouze@users.noreply.github.com>
2019-01-29 14:50:38 +0100

    Update mainFooter.blade.php

--- bde-cesi-nice/resources/views/default/mainFooter.blade.php
+++ bde-cesi-nice/resources/views/default/mainFooter.blade.php
@@ -4,18 +4,7 @@
 			Toute l'actualité sur <a href="https://www.facebook.com/BdeCesiNice/?ref=br_rs">notre page facebook</a>
 		</div>
 
-<<<<<<< HEAD
-		<div class="col-sm">
-			Nos partenaires :
-		</div>
 
-		<div class="col-sm">
-			Les membres du BDE 2019 :
-		</div>
-
-	</div>
-</div>
-=======
 	
 			A savoir :<br>
 			<a href="/Projet_Web_A2/bde-cesi-nice_vincent/public/legal">Mentions légales</a>
@@ -27,4 +16,3 @@
 
 	</div>
 </div>
->>>>>>> master
